e19696166b5e8a7e284203c82d3951fc8651653f
Fzz1n <166957195+Fzz1n@users.noreply.github.com>
2024-11-20 09:39:37 +0100

    data from US election 2016

---
+++ Scan_Data_TXT.c
@@ -0,0 +1,5 @@
+//
+// Created by 02fre on 20-11-2024.
+//
+
+#include "Scan_Data_TXT.h"
---
+++ Scan_Data_TXT.h
@@ -0,0 +1,8 @@
+//
+// Created by 02fre on 20-11-2024.
+//
+
+#ifndef SCAN_DATA_TXT_H
+#define SCAN_DATA_TXT_H
+
+#endif //SCAN_DATA_TXT_H
