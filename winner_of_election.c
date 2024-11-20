71a95160d9903554d99c3b9773b3859afbecda02
Fzz1n <166957195+Fzz1n@users.noreply.github.com>
2024-11-20 12:50:16 +0100

    make a folder to data.txt files

--- winner_of_election.c
+++ winner_of_election.c
@@ -5,7 +5,13 @@ char* Winner_of_election(states* USA) {
     double democrats = 0, republicans = 0, third_party = 0;
 
     //count electors
-
+    for (int i = 0; i < STATES; i++) {
+        if (USA[i].democrats > USA[i].republicans) {
+            democrats += USA[i].electors;
+        } else {
+            republicans += USA[i].electors;
+        }
+    }
     //
 
     //return winner;
