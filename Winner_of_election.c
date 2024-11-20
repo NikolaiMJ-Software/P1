35e7e04e7d08cfd0a6d14ff0f41426aaf62067f7
Fzz1n <166957195+Fzz1n@users.noreply.github.com>
2024-11-20 12:29:21 +0100

    test

--- Winner_of_election.c
+++ Winner_of_election.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 
 char* Winner_of_election(states* USA) {
-    double democrats = 0, republicans = 0;
+    double democrats = 0, republicans = 0, third_party = 0;
 
+    //count electors
 
+    //
+
+    return winner;
 }
