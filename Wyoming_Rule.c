3d307c05d88c612d4dffee39fbea9d551136a14c
NikolaiMJ-Software <nmje24@student.aau.dk>
2024-11-21 16:23:34 +0100

    wyoming rule works 2

--- Wyoming_Rule.c
+++ Wyoming_Rule.c
@@ -4,11 +4,11 @@
 states* wyoming_rule(int input_year,states* USA){
     int total_population = 0;
     int wyoming_population = USA[50].population;
+    int new_total_elector_count = 0;
     for(int i=0;i<STATES;i++){
         total_population += USA[i].population;
-        USA[i].electors = USA[i].population/wyoming_population+2;
+        USA[i].electors = (USA[i].population/wyoming_population)+2;
+        new_total_elector_count += USA[i].electors;
     }
-    printf("total: %d\n",total_population);
-    int new_total_elector_count = total_population/wyoming_population+100;
     printf("The total population for the dataset %d: %d people, which means the new electoral college has %d electors\n",input_year, total_population, new_total_elector_count);
 }
