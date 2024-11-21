f0f77ea456bbdfb306ba93122312f66f7730239a
Sebastianplaygames <94458257+Sebastianplaygames@users.noreply.github.com>
2024-11-21 12:43:49 +0100

    start of PLPR

---
+++ PLPR.c
@@ -0,0 +1,3 @@
+//
+// Created by sebas on 21/11/2024.
+//
--- connecter.h
+++ connecter.h
@@ -15,5 +15,6 @@ typedef struct states {
 void ScanData_TXT(int input_year, states* USA);
 char* Winner_of_election(states* USA, char* system);
 char* electoral_college(states* USA);
+char* PLPR(states* USA);
 
 #endif //SCAN_DATA_TXT_H
