94787a90e372089f0098d4066e85254313ad88c9
Fzz1n <166957195+Fzz1n@users.noreply.github.com>
2024-11-22 10:38:34 +0100

    update monte carlo + 2020.txt

--- monte_carlo.c
+++ monte_carlo.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #define comparisons 50000
 
-void MC_first_case(int TOT_tp_votes);
+void MC_first_case(int TOT_tp_votes, int* new_DEM_votes, int* new_REP_votes);
 void MC_second_case(int TOT_DEM_votes, int TOT_REP_votes, int TOT_TP_votes);
 
 // Monte Carlo study
@@ -10,31 +10,33 @@ void monte_carlo(states* USA, int choice) {
     // restart rand
     srand(time(NULL));
 
-    int D_count = 0, R_count = 0, TP_count = 0, new_DEM_votes = 0, new_REP_votes = 0, new_TP_votes = 0,
-        TOT_DEM_votes = 0, TOT_REP_votes = 0, TOT_TP_votes = 0;
+    int TOT_DEM_votes = 0, TOT_REP_votes = 0, TOT_TP_votes = 0,
+        new_DEM_votes = 0, new_REP_votes = 0, new_TP_votes = 0;
     // calculate the total of the votes, from different parties
     for (int i = 0; i < STATES; i++) {
         TOT_TP_votes += USA[i].third_votes;
-    }
-    for (int i = 0; i < STATES; i++) {
         TOT_DEM_votes += USA[i].dem_votes;
-    }
-    for (int i = 0; i < STATES; i++) {
         TOT_REP_votes += USA[i].rep_votes;
     }
 
     if (choice == 1) {
-        MC_first_case(TOT_TP_votes);
+        MC_first_case(TOT_TP_votes, &new_DEM_votes, &new_REP_votes);
         // return result
     } else if (choice == 2) {
         MC_second_case(TOT_DEM_votes, TOT_REP_votes, TOT_TP_votes);
         // return result
     }
+    // TEST print return values
+    printf("New DEM votes: %d\n", new_DEM_votes);
+    printf("New REP votes: %d\n", new_REP_votes);
+    printf("New TP votes: %d\n", new_TP_votes);
 }
 
-void MC_first_case(int TOT_tp_votes) {
+void MC_first_case(int TOT_tp_votes, int* new_DEM_votes, int* new_REP_votes) {
     // CASE 1: if the third-party votes get lost, and have to chose between DEM og REP
-    int DEM_count = 0, REP_count = 0, new_REP_votes = 0, new_DEM_votes = 0;
+    int DEM_count = 0, REP_count = 0;
+    *new_DEM_votes = 0;
+    *new_REP_votes = 0;
     for(int i = 0; i < TOT_tp_votes; i++) {
         for(int j = 0; j < comparisons; j++) {
             int r_no = 0;
@@ -46,10 +48,12 @@ void MC_first_case(int TOT_tp_votes) {
             }
         }
         if (DEM_count < REP_count) {
-            new_REP_votes++;
+            *new_REP_votes++;
         } else {
-            new_DEM_votes++;
+            *new_DEM_votes++;
         }
+        DEM_count = 0;
+        REP_count = 0;
     }
     // return TP secund choice
 }
@@ -74,8 +78,8 @@ void MC_second_case(int TOT_DEM_votes, int TOT_REP_votes, int TOT_TP_votes) {
             new_TP_votes++;
         }
     }
-    //return new votes
 
+    D_count = 0, TP_count = 0;
     // REP
     for(int i = 0; i < TOT_REP_votes; i++) {
         for(int j = 0; j < comparisons; j++) {
@@ -93,7 +97,8 @@ void MC_second_case(int TOT_DEM_votes, int TOT_REP_votes, int TOT_TP_votes) {
             new_TP_votes++;
         }
     }
-    // return new votes
 
     MC_first_case(TOT_TP_votes);
+
+    // return teh new votes
 }
