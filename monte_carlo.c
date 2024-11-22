2bd8f31efd9b751661544693d825929ef833df87
Fzz1n <166957195+Fzz1n@users.noreply.github.com>
2024-11-22 11:25:40 +0100

    update monte carlo + comments

--- monte_carlo.c
+++ monte_carlo.c
@@ -8,7 +8,7 @@ void MC_second_case(int TOT_DEM_votes, int TOT_REP_votes, int TOT_TP_votes,
 
 // Monte Carlo study of the voters second preference
 void monte_carlo(states* USA, int choice, int* new_DEM_votes, int* new_REP_votes, int* new_TP_votes) {
-    // Make thE randomize based on the time
+    // Make the randomize based on the time
     srand(time(NULL));
 
     int TOT_DEM_votes = 0, TOT_REP_votes = 0, TOT_TP_votes = 0;
@@ -60,7 +60,7 @@ void MC_first_case(int TOT_tp_votes, int* new_DEM_votes, int* new_REP_votes) {
 // CASE 2: All voters second preference
 void MC_second_case(int TOT_DEM_votes, int TOT_REP_votes, int TOT_TP_votes,
                     int* new_DEM_votes, int* new_REP_votes, int* new_TP_votes) {
-    int D_count = 0, R_count = 0, TP_count = 0;
+    int DEM_count = 0, REP_count = 0, TP_count = 0;
     // Reset the new votes for all parties
     *new_DEM_votes = 0;
     *new_REP_votes = 0;
@@ -71,41 +71,40 @@ void MC_second_case(int TOT_DEM_votes, int TOT_REP_votes, int TOT_TP_votes,
             // Random choice between REP (0) and TP (1,2)
             int r_no = rand() % 3;
             if (r_no == 0) {
-                R_count++;
+                REP_count++;
             } else {
                 TP_count++;
             }
         }
         // Add the vote to the party with most amount of votes
-        if (TP_count < R_count) {
+        if (TP_count < REP_count) {
             (*new_REP_votes)++;
         } else {
             (*new_TP_votes)++;
         }
         TP_count = 0;
-        R_count = 0;
+        REP_count = 0;
     }
 
-    D_count = 0, TP_count = 0;
     // REP voters second preference
     for(int i = 0; i < TOT_REP_votes; i++) {
         for(int j = 0; j < N_OF_SIMULATIONS; j++) {
             // Random choice between DEM (0) and TP (1,2)
             int r_no = rand() % 3;
             if (r_no == 3) {
-                D_count++;
+                DEM_count++;
             } else {
                 TP_count++;
             }
         }
-        if (TP_count < D_count) {
+        if (TP_count < DEM_count) {
             (*new_DEM_votes)++;
         } else {
             (*new_TP_votes)++;
         }
         // Add the vote to the party with most amount of votes
         TP_count = 0;
-        D_count = 0;
+        DEM_count = 0;
     }
     // run first case, to find third party second preference
     MC_first_case(TOT_TP_votes, new_DEM_votes, new_REP_votes);
