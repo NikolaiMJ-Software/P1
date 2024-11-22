3714067bba398452ef8728e06366bc6e5aa921b9
NikolaiMJ-Software <nikolai456654@gmail.com>
2024-11-22 12:33:42 +0100

    BC extended no return value

--- election_systems/BC.c
+++ election_systems/BC.c
@@ -1,13 +1,23 @@
 #include "../connecter.h"
 
 char* BC(states* USA) {
-    int democrats = 0, republicans = 0, third_party = 0;
-
+    int total_votes = 0, total_votes_dem = 0, total_votes_rep = 0, total_votes_third = 0;
+    for (int i = 0; i<STATES; i++) {
+        total_votes += USA[i].dem_votes+USA[i].rep_votes+USA[i].third_votes;
+        total_votes_dem += USA[i].dem_votes;
+        total_votes_rep += USA[i].rep_votes;
+        total_votes_third += USA[i].third_votes;
+    }
+    double dem_points = 0, rep_points = 0, tp_points = 0;
+    // Call function Monte Carlo to define second choice
     int new_DEM_votes_2nd = 0, new_REP_votes_2nd = 0, new_TP_votes_2nd = 0;
-    int new_DEM_votes_3nd = 0, new_REP_votes_3nd = 0, new_TP_votes_3nd = 0;
-    // Call function, "1" for "case 1" (ONLY third party second preference) and "2" for "case 2" (All second preference)
     monte_carlo(USA, 2, &new_DEM_votes_2nd, &new_REP_votes_2nd, &new_TP_votes_2nd);
-    monte_carlo(USA, 2, &new_DEM_votes_3nd, &new_REP_votes_3nd, &new_TP_votes_3nd);
+    //define third choice
+    int new_DEM_votes_3nd = 0, new_REP_votes_3nd = 0, new_TP_votes_3nd = 0;
+    new_DEM_votes_3nd = total_votes - new_DEM_votes_2nd-total_votes_dem;
+    new_REP_votes_3nd = total_votes - new_REP_votes_2nd-total_votes_rep;
+    new_TP_votes_3nd =  total_votes - new_TP_votes_2nd-total_votes_third;
+
     // Print return values from Monte Carlo
     printf("New DEM votes: %d\n", new_DEM_votes_2nd);
     printf("New REP votes: %d\n", new_REP_votes_2nd);
@@ -17,6 +27,20 @@ char* BC(states* USA) {
     printf("New REP votes: %d\n", new_REP_votes_3nd);
     printf("New TP votes: %d\n", new_TP_votes_3nd);
 
-    // Return winning party
+    //Calculate the amount of points from the different votes
+    for (int i = 0; i < STATES; i++) {
+        dem_points = USA[i].dem_votes+(0.5*new_DEM_votes_2nd)+(0.33*new_DEM_votes_3nd);
+        rep_points = USA[i].rep_votes+(0.5*new_REP_votes_2nd)+(0.33*new_REP_votes_3nd);
+        tp_points = USA[i].third_votes+(0.5*new_TP_votes_2nd)+(0.33*new_TP_votes_3nd);
+    }
+    printf("DEM points: %lf\n", dem_points);
+    printf("REP points: %lf\n", rep_points);
+    printf("TP points: %lf\n", tp_points);
+    printf("Total votes: %d\n", total_votes);
+    printf("Total votes_dem: %d\n", total_votes_dem+new_DEM_votes_2nd+new_DEM_votes_3nd);
+    printf("Total votes_dem: %d\n", total_votes_rep+new_REP_votes_2nd+new_REP_votes_3nd);
+    printf("Total votes_dem: %d\n", total_votes_third+new_TP_votes_2nd+new_TP_votes_3nd);
 
+    // Return winning party
+    return "democrats";
 }
