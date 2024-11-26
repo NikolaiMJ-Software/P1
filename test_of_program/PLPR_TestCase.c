a46a5748646c0752cac983c05f3344c774ed5e1f
Sebastianplaygames <94458257+Sebastianplaygames@users.noreply.github.com>
2024-11-26 11:18:29 +0100

    added case 4 for PLPR

--- test_of_program/PLPR_TestCase.c
+++ test_of_program/PLPR_TestCase.c
@@ -3,11 +3,6 @@
 #include "../monte_carlo.c"
 #include <assert.h>
 
-void test_do_next_op_case3(void);
-void test_do_next_op_case4(void);
-void test_do_next_op_case5(void);
-
-
 void test_do_next_op_case3(void) {
     states USA[STATES];
 
@@ -57,6 +52,7 @@ void test_do_next_op_case5(void) {
     //char wyoming_rule_true = 'n';
 
     // Populate the USA array
+    ScanData_TXT(2016, USA);
     wyoming_rule(2016, USA);
 
     // Call PLPR with the populated dataset
@@ -74,4 +70,29 @@ void test_do_next_op_case5(void) {
     assert(strcmp(result, expected_result) == 0);
 
     printf("PLPR Test case 3 passed!\n");
+}
+
+void test_do_next_op_case6(void) {
+    states USA[STATES];
+    //char wyoming_rule_true = 'n';
+
+    // Populate the USA array
+    ScanData_TXT(2024, USA);
+    wyoming_rule(2024, USA);
+
+    // Call PLPR with the populated dataset
+    char* result = PLPR(USA);
+
+    // Check if result is not NULL
+    assert(result != NULL);
+    //assert(&wyoming_rule_true == 'y');
+
+    // Check the expected result
+    char* expected_result = "Republicans"; // Modify this based on the expected winner for your data
+
+    // Print the result and assert the outcome
+    printf("Expected: '%s', Got: '%s'\n", expected_result, result);
+    assert(strcmp(result, expected_result) == 0);
+
+    printf("PLPR Test case 4 passed!\n");
 }
