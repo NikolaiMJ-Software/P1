3128cc9421c27225a45d53f5f3ec30fc3228d860
Alenje2004 <alenje@hotmail.com>
2024-11-26 11:53:33 +0100

    funny

--- Parameters.c
+++ Parameters.c
@@ -2,3 +2,13 @@
 // Created by nikol on 26-11-2024.
 //
 #include "connecter.h"
+int proportionality, minority_representation, party_system_concentration, personalization, legitimacy, comprehensibility;
+void good();
+function() {
+    if (proportionality) {
+        good();
+    }
+}
+void good() {
+    printf("System is good");
+}
