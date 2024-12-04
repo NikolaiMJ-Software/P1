ea52afab423796594deae128671b76801ef07f12
Fzz1n <166957195+Fzz1n@users.noreply.github.com>
2024-12-04 13:19:10 +0100

    new opdate compare_table

--- winner_of_election.c
+++ winner_of_election.c
@@ -9,7 +9,7 @@ char* Winner_of_election(states* USA, cmp* e_systems, cmp* uncap_systems, char* 
             for (int i = 0; i < NO_SYSTEMS; i++) {
                 if (strcmp(uncap_systems[i].system_name, system) == 0) {
                     already_calculated = 1;
-                    *counter_CMP = i;
+                    *counter_CMP++;
                     break;
                 }
             }
@@ -18,7 +18,7 @@ char* Winner_of_election(states* USA, cmp* e_systems, cmp* uncap_systems, char* 
             for (int i = 0; i < NO_SYSTEMS; i++) {
                 if (strcmp(e_systems[i].system_name, system) == 0) {
                     already_calculated = 1;
-                    *counter_CMP = i;
+                    *counter_CMP++;
                     break;
                 }
             }
