aa7f2e8a3d48dbaa141e59106383ed3d6d67631b
Fzz1n <166957195+Fzz1n@users.noreply.github.com>
2024-12-05 13:50:45 +0100

    fix fix states_abolished

--- winner_of_election.c
+++ winner_of_election.c
@@ -57,7 +57,6 @@ char* Winner_of_election(states* USA, candidates* candidate_list, cmp* e_systems
             // Start from the beginning of the while loop
             continue;
         }
-        printf("\n\n\n%d\n\n\n", states_abolished);
         if (strcmp(system, "original") == 0) {
             return electoral_college(USA, e_systems, uncap_systems, input_year, uncapped, states_abolished);
         } else if (strcmp(system, "STV") == 0) {
